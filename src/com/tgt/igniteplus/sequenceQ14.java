f49ff184fcda7a54ee41e9e91e9358c5e559a3e5
unknown <gmcharitha@gmail.com>
2020-03-19 16:44:42 +0530

    updated

--- src/com/tgt/igniteplus/sequenceQ14.java
+++ src/com/tgt/igniteplus/sequenceQ14.java
@@ -1,4 +1,21 @@
 package com.tgt.igniteplus;
 
-public class sequenceQ14 {
+import java.util.*;
+public class sequenceQ14
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the number of terms");
+        int n=in.nextInt();
+        int a[]=new int [n+3];
+        a[0]=1;
+        a[1]=4;
+        a[2]=7;
+        for(i=3;i<=n;i++)
+            a[i]=a[i-1]+a[i-2]+a[i-3];
+        for(i=0;i<=n;i++)
+            System.out.print(a[i]+" ");
+    }
 }
